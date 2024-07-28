84411a80bac982a770ae80915a2d424c0abc0083
Saurav Shandilya <108254690+shandilyasaurav405@users.noreply.github.com>
2024-07-28 23:04:35 +0530

    Task 2 completed

--- Day14/script.js
+++ Day14/script.js
@@ -11,11 +11,21 @@ class Person {
     getGreeting() {
       return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
     }
-  }
   
-  // Create an instance of the class
-  const person = new Person('raju', 30);
   
-  // Log the greeting message
-  console.log(person.getGreeting());
-  
+ 
+ 
+
+  // Task 2 : Add a method to the Person  class that updates the age property and logs the updated age.
+
+  updateAge(newAge) {
+    this.age = newAge;
+    console.log(`Updated age: ${this.age}`);
+}
+}
+
+const person = new Person("John", 30);
+
+console.log(person.getGreeting());
+
+person.updateAge(31);
